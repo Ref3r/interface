b29839d9d3e2b1310c86e8442ec9bc0da3dad280
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-04-08 05:01:19 +0530

    resolved merge conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,18 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+
 import LandingNavbar from "@/components/landing/LandingNavbar";
 import Hero from "@/components/landing/HeroSection";
 import Features from "@/components/landing/FeaturesSection";
 import OfferSection from "@/components/landing/OfferSection";
 import HowItWorks from "@/components/landing/HowItWorks";
-import appwriteService from "@/appwrite/config";
-
 import Footer from "@/components/landing/Footer";
-
-
+import appwriteService from "@/appwrite/config";
 import { usePublicKey } from "@/store";
 
+
 export default function Home() {
-  const router = useRouter();
-  //const key = "0x953ed43e99938fDD2B0c91E4521Cccc2762aF70A";
-  const key = usePublicKey.getState().publicKey;
-  function updatePublicKey(key: string) {
-    usePublicKey.setState({ publicKey: key });
-  }
-  updatePublicKey("0x4953ed43e99938fDD2B0c91E4521Cccc2762aF70A");
-  async function createUser(key: string) {
-    const user = await appwriteService.createUserAccount(key);
-  }
-  async function checkUserExist() {
-    console.log("checking if user exists");
-    try {
-      const data = await appwriteService.getCurrentUser().then();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log("user created");
-      await createUser(key);
-      router.push("/dashboard");
-    }
-  }
-  useEffect(() => {
-    const userCheck = async () => {
-      const user = await checkUserExist();
-      console.log(user);
-      if (user) {
-        router.push("/dashboard");
-      }
-    };
-    userCheck();
-  }, []);
   return (
     <main className="bg-[#111111] flex min-h-screen flex-col items-center justify-between">
       <div
